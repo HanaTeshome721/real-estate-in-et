7e0a5b0400e3a1b1c9f7478f18ec0593dd511f18
HanaTeshome721 <hana2729tm@gmail.com>
2025-10-07 14:36:12 +0300

    feat: functions to be tested

---
+++ src/app/components/mathFunctions.tsx
@@ -0,0 +1,11 @@
+export const add = (a: number, b: number) => a + b;
+export const multiply = (a: number, b: number) => a * b;
+
+export default function MathComponent() {
+  return (
+    <div>
+      <p>Add: {add(2, 3)}</p>
+      <p>Multiply: {multiply(2, 3)}</p>
+    </div>
+  );
+}
---
+++ src/app/mathFunctions.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+const MathComponent: React.FC = () => (
+  <div>
+    <div>Add: {add(2, 3)}</div>
+    <div>Multiply: {multiply(2, 3)}</div>
+  </div>
+);
+
+export default MathComponent;
