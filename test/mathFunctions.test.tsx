141de82950e7693d2d245209e702e7d0e1809572
HanaTeshome721 <hana2729tm@gmail.com>
2025-10-08 14:17:11 +0300

    add: testcase edit

--- test/mathFunctions.test.tsx
+++ test/mathFunctions.test.tsx
@@ -6,6 +6,7 @@ describe('Math Functions', () => {
   test('add function works', () => {
     expect(add(2, 3)).toBe(5);
     expect(add(-1, 1)).toBe(0);
+    expect(add(-1, 1)).toBe(2);
   });
 
   test('multiply function works', () => {
