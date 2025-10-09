8c46beb8602e571b9a0ee6a551676b800b812379
HanaTeshome721 <hana2729tm@gmail.com>
2025-10-09 17:01:47 +0300

    udate :math

--- src/app/components/mathFunctions.tsx
+++ src/app/components/mathFunctions.tsx
@@ -4,8 +4,8 @@ export const multiply = (a: number, b: number) => a * b;
 export default function MathComponent() {
   return (
     <div>
-      <p>Add: {add(2, 3)}</p>
-      <p>Multiply: {multiply(2, 3)}</p>
+      <p>Add:{add(2, 3)}</p>
+      <p>Multiply:{multiply(2, 3)}</p>
     </div>
   );
 }
