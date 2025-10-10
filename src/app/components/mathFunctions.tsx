3dafd9a4bc155e04a6c97573844d520d8505ac06
HanaTeshome721 <hana2729tm@gmail.com>
2025-10-10 13:13:44 +0300

    feat:add func math fun

--- src/app/components/mathFunctions.tsx
+++ src/app/components/mathFunctions.tsx
@@ -5,7 +5,7 @@ export default function MathComponent() {
   return (
     <div>
       <p>Add:{add(2, 3)}</p>
-      <p>Multiply:{multiply(2, 3)}</p>
+      <p>Multiply  :{multiply(2, 3)}</p>
     </div>
   );
 }
